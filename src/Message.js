1ef9e4e3b6fa58ad359ba1b6368901c23fd616c1
feven tsegay <feventsegay6@gmail.com>
2019-07-05 14:25:12 -0500

    done

--- src/Message.js
+++ src/Message.js
@@ -1,1 +1,37 @@
+class Message{
+    constructor(res){
+        this.res=res
+        this.render()
+        Message.all.push(this)
+        // setInterval(()=>{
+        //     fetch(`http://10.185.1.218:3000/messages/${res.id}`)
+        //     .then(res => res.json())
+        //     .then(data=>{
+                
+        //         this.allMessages()
+        //     })
+        // }, 5000)
 
+    }
+    render(){
+        let ul = document.getElementById('messages')
+        let li = document.createElement('li')
+        li.innerHTML=this.res.content
+        ul.append(li)
+    }
+    static destroyAll(){
+        // this === Character
+        Message.all.forEach( message => message.remove() )
+    }
+    // allMessages(){
+    //     let ul = document.getElementById('messages')
+    //     ul.innerHTML = ''
+    //     for(let i = 0; i < Message.all.length; i++){
+    //         let li = document.createElement('li')
+    //         li.innerHTML=Message.all[i].content
+    //         console.log(Message.all[i])
+    //         ul.append(li)
+    //     }
+    // }
+    static all = []
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,48 @@
+function newMessages(){
+    setInterval(()=>{
+    fetch(`http://10.185.1.218:3000/messages`)
+    .then(res => res.json())
+    .then(data=>{              
+        let ul = document.getElementById('messages')
+        ul.innerHTML =" "
+        console.log(ul)
+        for(let i = 0; i<data.length; i++){
+            let li = document.createElement('li')
+            li.innerHTML=data[i].content
+            ul.append(li)
+        }
+    })
+    }, 500)
+}
 
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetch("http://10.185.1.218:3000/messages")
+    .then(function(response){return response.json()})
+    .then(function(res){
+        newMessages()
+        let text = document.querySelector('#message_input')
+        let submitBtn =  document.querySelector('#submitBtn')
+        for(let i=0; i< res.length; i++){
+            new Message(res[i])
+        }
+        submitBtn.addEventListener('click',(e)=>{
+            e.preventDefault()
+            fetch("http://10.185.1.218:3000/messages",{
+                method: "POST",
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    content: text.value
+                })
+            })
+            .then(res => res.json())
+            .then(data=>{
+                new Message(data)
+            })
+        })
+    })
+})
